6da58ffb18687c64dca620145457bd3852d1953d
sohid7254 <sohidameen321@gmail.com>
2025-08-13 19:52:25 +0600

    Problem-1 Done

---
+++ problem-1.js
@@ -0,0 +1,12 @@
+// Problem-01 : Train TT's Fine Calculator 
+function totalFine(fare){
+    if (typeof fare !== 'number' ||  fare <=0){
+        return "Invalid";
+    }
+    let surCharge = fare * (20/100);
+    let serviceCharge = 30;
+    let fine = fare + surCharge + serviceCharge;
+    return fine;
+}
+
+// console.log(totalFine(200));
